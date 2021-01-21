055448e9fb1064a85ac323a1bc261d2e369802a3
Quintonn Rothmann <quintonnr@gmail.com>
2021-01-21 14:14:25 +0200

    changed

--- example-smart-app/src/js/main.js
+++ example-smart-app/src/js/main.js
@@ -9,7 +9,7 @@
         div.id = 'codeData';
         div.innerText = window.location.search;
         div.style.display = 'none';
-        document.appendChild(div);
+        document.body.appendChild(div);
 
         //window.location.href = 'https://a9e3fef6f57b.ngrok.io/api/test' + window.location.search + '&arg=1';
         window.close();
