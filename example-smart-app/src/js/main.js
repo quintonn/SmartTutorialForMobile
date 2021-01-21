27398fc554df86ec4c5ac4ddcb083460e6e4f34d
Quintonn Rothmann <quintonnr@gmail.com>
2021-01-21 13:49:12 +0200

    fix redirect

--- example-smart-app/src/js/main.js
+++ example-smart-app/src/js/main.js
@@ -3,9 +3,7 @@
     setTimeout(function()
     {
         alert('redirecting');
-        alert(window.location);
-        alert(window.location.href);
         //window.location.href = 'https://a9e3fef6f57b.ngrok.io/api/test';
-        window.location = 'https://a9e3fef6f57b.ngrok.io/api/test' + window.location.search + '&arg=1'
+        window.location.href = 'https://a9e3fef6f57b.ngrok.io/api/test' + window.location.search + '&arg=1';
     }, 3000);
 })(window);
