610b0abad4cb2629f7f4223e001b4049cc3f3070
Quintonn Rothmann <quintonnr@gmail.com>
2021-01-21 14:26:18 +0200

    changed

--- example-smart-app/src/js/main.js
+++ example-smart-app/src/js/main.js
@@ -2,7 +2,6 @@
     console.log('ready');
     setTimeout(function()
     {
-        alert('Processing...');
         //window.location.href = 'https://a9e3fef6f57b.ngrok.io/api/test';
         
         var div = document.createElement('div');
@@ -13,6 +12,6 @@
 
         //window.location.href = 'https://a9e3fef6f57b.ngrok.io/api/test' + window.location.search + '&arg=1';
         //window.close();
-        window.postMessage({event: 'done', data: window.location.search});
-    }, 3000);
+        window.opener.postMessage('xxx');//{event: 'done', data: window.location.search});
+    }, 1000);
 })(window);
