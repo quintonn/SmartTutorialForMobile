055623cad291f1ace4c2ce311b8bf3584580295d
Quintonn Rothmann <quintonnr@gmail.com>
2021-01-21 13:40:37 +0200

    add alert

--- example-smart-app/src/js/main.js
+++ example-smart-app/src/js/main.js
@@ -2,9 +2,9 @@
     console.log('ready');
     setTimeout(function()
     {
-        console.log('redirecting');
-        console.log(window.location);
-        console.log(window.location.href);
+        alert('redirecting');
+        alert(window.location);
+        alert(window.location.href);
         //window.location.href = 'https://a9e3fef6f57b.ngrok.io/api/test';
         window.location = 'https://a9e3fef6f57b.ngrok.io/api/test' + window.location.search + '&arg=1'
     }, 3000);
