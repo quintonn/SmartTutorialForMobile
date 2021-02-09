c0417e60a948f1063dd90e4216d6bec4eb932f8c
Quintonn Rothmann <quintonnr@gmail.com>
2021-02-09 11:59:51 +0200

    update qms code

--- example-smart-app/src/js/main.js
+++ example-smart-app/src/js/main.js
@@ -1,11 +1,4 @@
 (function(window){
-    console.log('ready');
-    console.log('');
-    console.log('------------------------------------------------------------');
-    console.log(window.location.href);
-    console.log('------------------------------------------------------------');
-    console.log('');
-
     var state = '';
     var queryParams = window.location.search.substring(1).split('&');
     for (var i = 0; i < queryParams.length; i++)
@@ -18,27 +11,14 @@
         }
     }
 
-    console.log('state = ' + state);
-
     var sessionState = sessionStorage[state];
-    console.log(sessionState);
-    console.log(btoa(sessionState));
-   
+
     setTimeout(function()
     {
-        console.log(window.location.search);
-        window.location.href = '/close' + window.location.search + '&sessionstate='+btoa(sessionState);
+        //window.location.href = '/close' + window.location.search + '&sessionstate='+btoa(sessionState);
+        window.location.href = 'https://quintonn.github.io/SmartTutorialForMobile/qms/index.html' + window.location.search + '&sessionstate='+btoa(sessionState) + "&close=true";
+
         //window.location.href = 'https://quintonn.github.io/SmartTutorialForMobile/qms/index.html' + window.location.search + '&sessionstate='+btoa(sessionState);
     }, 10);
-    // FHIR.oauth2.ready(onReady, onError);
-
-    // function onReady()
-    // {
-    //     console.log('on ready');
-    // }
-
-    // function onError()
-    // {
-    //     console.log('onError');
-    // }
+    
 })(window);
